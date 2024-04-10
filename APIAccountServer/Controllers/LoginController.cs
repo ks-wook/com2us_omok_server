deb57460148dc6c9379bad0d8eb2ca4471a8fc74
ks-wook <ksu864@gmail.com>
2024-04-10 22:14:17 +0900

    init controller dir

---
+++ APIAccountServer/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIAccountServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
