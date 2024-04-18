3e52384aa4b4913686821049c691ecc7cc6b3145
ks-wook <ksu864@gmail.com>
2024-04-18 17:00:58 +0900

    이메일 형식 체크를 annotation 방식으로 변경

--- HiveServer/Model/DTO/Login.cs
+++ HiveServer/Model/DTO/Login.cs
@@ -1,13 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
+// annotation 방식 이메일 형식 검사
 namespace HiveServer.Model.DTO
 {
     public class LoginReq
     {
-        [Required]
+        [Required(ErrorMessage = "EMAIL CANNOT BE EMPTY")]
+        [MinLength(1, ErrorMessage = "EMAIL CANNOT BE EMPTY")]
+        [StringLength(50, ErrorMessage = "EMAIL IS TOO LONG")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "PASSWORD CANNOT BE EMPTY")]
+        [MinLength(1, ErrorMessage = "PASSWORD CANNOT BE EMPTY")]
+        [StringLength(30, ErrorMessage = "PASSWORD IS TOO LONG")]
         public string Password { get; set; } = string.Empty;
     }
 
