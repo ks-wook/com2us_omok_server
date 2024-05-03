05935773d368820ab03f1f272028fd59a0ac3ae9
ks-wook <ksu864@gmail.com>
2024-05-03 12:42:08 +0900

    ThirdParty Dir 추가

---
+++ SuperSocketLite/IAsyncSocketSession.cs
@@ -0,0 +1,24 @@
+﻿using SuperSocket.Common;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Logging;
+using SuperSocket.SocketEngine.AsyncSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SuperSocket.SocketEngine
+{
+    internal interface IAsyncSocketSessionBase : ILoggerProvider
+    {
+        SocketAsyncEventArgsProxy SocketAsyncProxy { get; }
+
+        Socket Client { get; }
+    }
+
+    internal interface IAsyncSocketSession : IAsyncSocketSessionBase
+    {
+        void ProcessReceive(SocketAsyncEventArgs e);
+    }
+}
